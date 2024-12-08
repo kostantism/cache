491ef837dc85149f8bfa1c8914ac7363108ee8f2
kostantis <smavraganiskostantis@gmail.com>
2024-12-08 15:58:11 +0200

    first commit

---
+++ src/main/java/org/hua/cache/App.java
@@ -0,0 +1,9 @@
+package org.hua.cache;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+    }
+}
---
+++ src/main/java/org/hua/cache/LRUCache.java
@@ -0,0 +1,13 @@
+package org.hua.cache;
+
+public class LRUCache<K, V> implements Cache{
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+
+    }
+}
